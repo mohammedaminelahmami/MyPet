fe2ef2a0cb0b57369e85f6962da6b6b2f2cb07b7
mohammedaminelahmami <lahmamimohammedamine@gmail.com>
2022-12-31 16:49:18 +0100

    first

---
+++ src/main/java/com/youcode/mypet/MypetApplication.java
@@ -0,0 +1,13 @@
+package com.youcode.mypet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MypetApplication.class, args);
+    }
+
+}
